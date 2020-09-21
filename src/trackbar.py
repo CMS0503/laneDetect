665a0a559cf923c2647695618d17c866b9a109e3
CMS0503 <tyms0503@gmail.com>
2020-09-21 20:41:12 +0900

    Add video version

--- src/trackbar.py
+++ src/trackbar.py
@@ -20,17 +20,37 @@ cv2.createTrackbar('high threshold', 'bin', 0, 255, nothing)
 cv2.setTrackbarPos('low threshold', 'bin', 50)
 cv2.setTrackbarPos('high threshold', 'bin', 150)
 
-cv2.imshow("Original", img)
+# cv2.imshow("Original", img)
+
+cap = cv2.VideoCapture('../challenge_video.mp4')
+        # while(cap.isOpened()):
 
 while True:
+    _, frame = cap.read()
 
     low = cv2.getTrackbarPos('low threshold', 'bin')
     high = cv2.getTrackbarPos('high threshold', 'bin')
     s_thresh = (low, high)
-    img_binary = find_edges(img, s_thresh)
-    cv2.imshow("bin", img_binary)
+    img_binary = find_edges(frame, s_thresh)
 
-    if cv2.waitKey(1)&0xFF == 27:
+    output = np.zeros((720, 1400, 3), dtype=np.uint8)
+
+    frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    img_binary = cv2.resize(img_binary, (0, 0), fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+
+    img_binary = np.reshape(img_binary, (360, 640, 1))
+    print(img_binary.shape)
+    w = frame.shape[1]
+    h = frame.shape[0]
+
+    output[40:40+h, 20:20+w, :] = frame
+    output[40:40+h, 20*2+w:20*2+w*2, :] = img_binary
+
+    cv2.imshow("bin", output)
+
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.waitKey(0)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 
